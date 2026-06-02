544b1491b3cce12166c940ffc80e1a4dc0f0235a
itzHassanSH <mhshahzadpk@gmail.com>
2026-06-03 01:47:58 +0200

    startingsmessage
    with '#' will be ignored, and an empty message aborts the commit.

---
+++ spring-boot/src/main/java/com/tracker/entity/Job.java
@@ -0,0 +1,11 @@
+package com.tracker.entity;
+
+
+// Jakarta Persistence is the standard specification for working with databases in Java
+// Spring Data JPA Dependency required for this package - Spring Boot + JPA (or Hibernate)
+
+public class Job {
+    public Job () {
+
+    }
+}
--- spring-boot/src/test/java/com/tracker/ApplicationTests.java
+++ spring-boot/src/test/java/com/tracker/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.weather;
+package com.tracker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
