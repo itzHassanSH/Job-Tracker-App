b4bab879c73ac01a1b82d003628c510c7679528f
itzHassanSH <mhshahzadpk@gmail.com>
2026-06-02 22:45:11 +0200

    move to monorepo structure

--- spring-boot/src/main/java/com/weather/Application.java
+++ spring-boot/src/main/java/com/weather/Application.java
@@ -2,12 +2,20 @@ package com.weather;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+    // This here is a REST Endpoint
+    @GetMapping("/")
+    public String home() {
+        return "broski";
+    }
 }
