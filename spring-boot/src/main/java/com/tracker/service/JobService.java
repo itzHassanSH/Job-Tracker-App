2dea33282c40e9c5144d216c9c2309223d5611c3
itzHassanSH <mhshahzadpk@gmail.com>
2026-06-14 12:01:11 +0200

    added new database repo: JovEventRepository    - as an attribute inside JobService    - actual repository interface inside package: Repository

--- spring-boot/src/main/java/com/tracker/service/JobService.java
+++ spring-boot/src/main/java/com/tracker/service/JobService.java
@@ -7,20 +7,25 @@ import com.tracker.entity.EventType;
 import com.tracker.entity.Job;
 import com.tracker.entity.JobEvent;
 import com.tracker.entity.Status;
+import com.tracker.repository.JobEventRepository;
 import com.tracker.repository.JobRepository;
 import lombok.NonNull;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class JobService {
 
-    private final JobRepository repo;
+    private final JobRepository jobRepository;
+    private final JobEventRepository jobEventRepository;
 
-    public JobService (JobRepository repo) {
-        this.repo = repo;
+    public JobService (JobRepository jobRepository, JobEventRepository jobEventRepository) {
+        // Repositories = doors into the database - not a database itself!
+        this.jobRepository = jobRepository;
+        this.jobEventRepository = jobEventRepository;
     }
 
     @NonNull
@@ -31,27 +36,49 @@ public class JobService {
                 .status(Status.APPLIED)
                 .location(request.location())
                 .contactPerson(request.contactPerson())
-                .notes(request.notes())
+                .title(request.title())
                 .build();
         JobEvent event = new JobEvent.Builder()
                 .type(EventType.APPLICATION_SENT)
                 .date(LocalDate.now())
                 .build();
         job.addEvent(event);
-        repo.save(job);
+        jobRepository.save(job);
+        jobEventRepository.save(event);
 
 
         ArrayList<JobEventResponse> eventResponses = new ArrayList<>();
         eventResponses.add(new JobEventResponse(event.getType(), event.getDate()));
         return new JobResponse(
                 job.getDescription(),
+                job.getId(),
                 job.getCompany(),
                 job.getLocation(),
                 job.getStatus(),
                 job.getContactPerson(),
-                job.getNotes(),
+                job.getTitle(),
                 eventResponses
         );
 
+    }
+
+    @NonNull
+    public List<JobResponse> findAllJobs() {
+        return jobRepository.findAll().stream()
+                .map(job -> new JobResponse(
+                        job.getDescription(),
+                        job.getId(),
+                        job.getCompany(),
+                        job.getLocation(),
+                        job.getStatus(),
+                        job.getContactPerson(),
+                        job.getTitle(),
+                        job.getEvents().stream()
+                                .map(jobEvent -> new JobEventResponse(
+                                        jobEvent.getType(),
+                                        jobEvent.getDate()
+                                ))
+                                .toList()
+                )).toList();
     }
 }
