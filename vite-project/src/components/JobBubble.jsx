fb72b71285545409dff63ec12ce5e67808a3c5ed
itzHassanSH <mhshahzadpk@gmail.com>
2026-06-21 10:48:00 +0200

    added functionality to JobBubble:    - allows for deleting jobs    - allows for editing jobs

--- vite-project/src/components/JobBubble.jsx
+++ vite-project/src/components/JobBubble.jsx
@@ -1,17 +1,34 @@
-import {useState} from "react";
+import {useState} from "react"
+import "../components/JobBubble.css"
+import JobsButtons from "./JobsButtons.jsx";
 
-function JobBubble( {job} ) {
+function JobBubble( {job, deleteMode, isSelected, onToggleSelection, handleEdit} ) {
     const [expanded, setExpanded] = useState(false);
 
+    // There's a big difference between handleEdit(job) and () => handleEdit(job)
+    // One is a function being passed (that executes onClick), while the other executes exactly then and there
     return (
-        <div onClick={() => setExpanded(!expanded)}>
-            <h3>{job.title}</h3>
-            <h3>{job.company}</h3>
-            <h3>{job.status}</h3>
-            <h3>{job.location}</h3>
+        <div className="job-bubble" >
+
+            {deleteMode && (
+                <input
+                    type="checkbox"
+                    checked={isSelected}
+                    onChange={() => onToggleSelection(job.id)}
+                />
+            )}
+
+            <div className="job-row" onClick={() => setExpanded(!expanded)}>
+                <span>{job.title}</span>
+                <span>{job.company}</span>
+                <span>{job.location}</span>
+                <span>{job.status}</span>
+            </div>
+            {!deleteMode && (<JobsButtons text={"Edit"} onClick={() => handleEdit(job)} className={"edit-button"}/>)}
 
             {expanded && (
                 <div>
+                    <p>Description: </p>
                     <p>{job.description}</p>
                 </div>
             )}
