a8693f917fa80f0be3097f1a952e96fa799b489c
itzHassanSH <mhshahzadpk@gmail.com>
2026-06-21 10:49:37 +0200

    JobService:    - Added API mappings for:       - deleting       - updating/editing

--- vite-project/src/services/JobService.js
+++ vite-project/src/services/JobService.js
@@ -9,3 +9,12 @@ export const createJobs = async (jobData) => {
     const response = await api.post("jobs", jobData);
     return response.data;
 }
+
+export const deleteJobs = async (jobId) => {
+    await api.delete(`jobs/${jobId}`);
+}
+
+export const updateJob = async (jobId, jobData) => {
+    const response = await api.patch(`jobs/${jobId}`, jobData);
+    return response.data;
+}
